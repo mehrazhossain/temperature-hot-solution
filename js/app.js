4715a20ba316ebe43d0e774d02f677a76ba41f75
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-03-02 16:43:20 +0600

    api data added

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@ const searchTemperature = () => {
     .then((data) => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = (temperature) => {
   console.log(temperature);
+  setInnerText('city', temperature.name);
+  setInnerText('temperature', temperature.main.temp);
+  setInnerText('condition', temperature.weather[0].main);
+  // Set weather icon
+  const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById('weather-icon');
+  imgIcon.setAttribute('src', url);
 };
